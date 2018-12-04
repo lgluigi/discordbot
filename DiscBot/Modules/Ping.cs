57c66f8c265633c9067c367496082f12cdca65a1
Luigi <luigilg@terra.com.br>
2018-12-03 23:59:34 -0200

    Add files via upload

---
+++ DiscBot/Modules/Ping.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using DiscBot;
+using Discord.WebSocket;
+
+namespace DiscBot.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("PingTest")
+                .WithDescription("Descrição de Ping")
+                .WithColor(Color.DarkBlue);
+            await ReplyAsync("", false, builder.Build());
+
+        }
+        //Get in voice-chat
+        [Command("colae", RunMode = RunMode.Async)]        
+        public async Task getIn ()
+        {
+            SocketGuildUser user = Context.User as SocketGuildUser;
+            if(user.VoiceState == null)
+            {
+                await ReplyAsync("Conecta num voice-chat ae mein ");
+                return;
+            }
+            ISocketAudioChannel channel = user.VoiceChannel;
+            Console.WriteLine(channel);
+            await channel.ConnectAsync();
+            
+            Console.WriteLine("Conectado");
+        }
+        
+        [Command("return")]
+        public async Task RetornarBuildAsync()
+        {
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.AddField("um teste","muito loco")
+                .AddInlineField("B","t")
+                .AddInlineField("C","e");
+
+        }
+
+        [Command("responde")]
+        public async Task RetornarFraseAsync(SocketGuildUser user)
+        {
+            //Deveria retornar a tag do usuario que chamou a função
+            EmbedBuilder builder = new EmbedBuilder();
+
+           await ReplyAsync($"O qq ce qué, {user.Mention }");
+
+        }
+        [Command("help")]
+        public async Task HelpCommand()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            await ReplyAsync("Comandos :'colae' 'ping' ");
+        }
+    }
+}
---
+++ DiscBot/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Discord;
+using System.IO;
+using Discord.Commands;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DiscBot
+{
+    class Program
+    {
+
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+        private IServiceProvider _services;
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        //SocketGuildUser (atributos e ações de usuario no discord)
+        public async Task RunBotAsync()
+        {
+
+            Client = new DiscordSocketClient();
+            Commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(Client)
+                .AddSingleton(Commands)
+                .BuildServiceProvider();
+
+            string botToken = "";
+
+
+            //event Subscriptions
+
+            Client.Log += Log;
+            //Client.UserJoined += AnnounceUserJoined;
+
+            await RegisterCommandsAsync();
+            // Loga no socket utilizando a token do Bot
+            await Client.LoginAsync(TokenType.Bot, botToken);
+
+            //Se conectando com os parametros do LoginAsync
+
+            await Client.StartAsync();
+
+            //comando looping para a aplicação não fechar após rodar o código
+            await Task.Delay(-1);
+
+
+        }
+
+        /*  private async Task AnnounceUserJoined(SocketGuildUser user)
+          {
+              var guild = user.Guild;
+              var channel = guild.DefaultChannel;
+              await channel.SendMessageAsync($"welcome,{user.Mention}");
+          }*/
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            //logs para mostrar no console
+            return Task.CompletedTask;
+        }
+        public async Task RegisterCommandsAsync()
+        {
+            Client.MessageReceived += HandleCommandAsync;
+            Console.WriteLine("public Async Task Register Command Async");
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+        }
+        //Função de Comandos e configuração de Prefixo
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            Console.WriteLine(arg);
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!lg ", ref argPos) || message.HasMentionPrefix(Client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(Client, message);
+
+                var result = await Commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+
+    }
+}
+
